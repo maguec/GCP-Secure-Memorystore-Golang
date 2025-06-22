4c396a9f08d13df714dc6fc2d06eca526b8216ae
Chris Mague <github@mague.com>
2025-06-22 07:26:10 -0700

    run a bunch of checkers

--- secure-pool-example.go
+++ secure-pool-example.go
@@ -1,23 +1,20 @@
 package main
 
 import (
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
+	"github.com/alexflint/go-arg"
+	"github.com/valkey-io/valkey-go"
 	"log"
 	"math/rand"
+	"net"
+	"os"
 	"sync"
 	"time"
-
-  "google.golang.org/api/option"
-	"cloud.google.com/go/auth/grpctransport"
-	secretmanager "cloud.google.com/go/secretmanager/apiv1"
-  credentials "google.golang.org/genproto/googleapis/iam/credentials/v1"
-	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
-	"github.com/alexflint/go-arg"
-	"github.com/golang/protobuf/ptypes"
-	"github.com/valkey-io/valkey-go"
 )
 
 type Rconf struct {
@@ -26,69 +23,15 @@ type Rconf struct {
 	Cert string
 }
 
-var (
-	mu                 sync.RWMutex
-	lastRefreshInstant = time.Time{}
-	errLastSeen        = error(nil)
-	token              = ""
-)
-
 var args struct {
-	Project                  string        `help:"GCP ProjectID" default:"" arg:"--project, -p, env:GCP_PROJECT"`
-	Instance                 string        `help:"Memorystore Instance name" default:"" arg:"--instance, -i, env:MEMORYSTORE_INSTANCE"`
-	Lifetime                 time.Duration `help:"Lifetime of token" default:"1h" arg:"--lifetime, -l, env:LIFETIME"`
-	RefreshDuration          time.Duration `help:"Refresh duration" default:"5m" arg:"--refresh-duration, -r, env:REFRESH_DURATION"`
-	CheckTokenExpiryInterval time.Duration `help:"Check token expiry interval" default:"10s" arg:"--check-token-expiry-interval, -c, env:CHECK_TOKEN_EXPIRY_INTERVAL"`
-}
-
-func refreshTokenLoop() {
-	if args.RefreshDuration > args.Lifetime {
-		log.Fatal("Refresh should not happen after token is already expired.")
-	}
-	for {
-		mu.RLock()
-		lastRefreshTime := lastRefreshInstant
-		mu.RUnlock()
-		if time.Now().After(lastRefreshTime.Add(args.RefreshDuration)) {
-			var err error
-			retrievedToken, err := retrieveTokenFunc(valkey.AuthCredentialsContext{})
-			mu.Lock()
-			token = retrievedToken
-			if err != nil {
-				errLastSeen = err
-			} else {
-				lastRefreshInstant = time.Now()
-			}
-			mu.Unlock()
-		}
-		time.Sleep(args.CheckTokenExpiryInterval)
-	}
+	Project  string `help:"GCP ProjectID" default:"" arg:"--project, -p, env:GCP_PROJECT"`
+	Instance string `help:"Memorystore Instance name" default:"" arg:"--instance, -i, env:MEMORYSTORE_INSTANCE"`
 }
 
 func retrieveTokenFunc(yo valkey.AuthCredentialsContext) (valkey.AuthCredentials, error) {
-  ctx := context.Background()
-  conn, err := grpctransport.Dial(
-    ctx,
-    option.WithEndpoint("iamcredentials.googleapis.com:443"),
-    option.WithScopes("https://www.googleapis.com/auth/cloud-platform"),
-    )
-  if err != nil {
-    log.Printf("Failed to call API: %v", err)
-    return valkey.AuthCredentials{}, err
-  }
-  client := credentials.NewIAMCredentialsClient(conn)
-  req := credentials.GenerateAccessTokenRequest{
-    Name: "projects/-/serviceAccounts/-",
-    Scope: []string{"https://www.googleapis.com/auth/cloud-platform"},
-    Lifetime: ptypes.DurationProto(args.Lifetime),
-  }
-  resp, err := client.GenerateAccessToken(ctx, req)
-  if err != nil {
-    log.Printf("Failed to call API: %v", err)
-    return valkey.AuthCredentials{}, err
-  }
 	username := "default"
-	return valkey.AuthCredentials{Username: username, Password: resp.AccessToken}, nil
+	password := os.Getenv("TOKEN")
+	return valkey.AuthCredentials{Username: username, Password: password}, nil
 }
 
 func getSecret(projectID string, secretID string) (Rconf, error) {
@@ -129,6 +72,13 @@ func getSecret(projectID string, secretID string) (Rconf, error) {
 	return cfg, nil
 }
 
+func valkeyDialLogger(ctx context.Context, s1 string, dialer *net.Dialer, tlsconfig *tls.Config) (net.Conn, error) {
+	now := time.Now()
+	conn, err := net.Dial("tcp", s1)
+	log.Printf("Dialing %s took %v", s1, time.Since(now))
+	return conn, err
+}
+
 func valkeyConfig(cfg Rconf) valkey.ClientOption {
 	caCertPool := x509.NewCertPool()
 	caCertPool.AppendCertsFromPEM([]byte(cfg.Cert))
@@ -139,9 +89,27 @@ func valkeyConfig(cfg Rconf) valkey.ClientOption {
 		TLSConfig: &tls.Config{
 			RootCAs: caCertPool,
 		},
-		SendToReplicas: func(cmd valkey.Completed) bool {
-			return (cmd.IsReadOnly() && rand.Intn(2) == 0)
-		},
+		DialCtxFn: valkeyDialLogger,
+	}
+}
+
+func worker(conf Rconf, i int) {
+	if i > 1024 {
+		time.Sleep(time.Duration(i/3) * time.Millisecond)
+	}
+	log.Printf("Worker %d started", i)
+	valkeyConf := valkeyConfig(conf)
+	rdb, err := valkey.NewClient(valkeyConf)
+	if err != nil {
+		log.Fatalf("configuration failed: %v", err)
+	}
+	defer rdb.Close()
+	for {
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+		err := rdb.Do(context.Background(), rdb.B().Ping().Build()).Error()
+		if err != nil {
+			log.Println(err)
+		}
 	}
 }
 
@@ -156,21 +124,12 @@ func main() {
 		log.Fatalf("Failed to get secret: %v", err)
 	}
 
-	conf := valkeyConfig(cfg)
-
-	ctx := context.Background()
-	rdb, err := valkey.NewClient(conf)
-	if err != nil {
-		log.Fatalf("configuration failed: %v", err)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	for i := 0; i < 10; i++ {
+		go func() {
+			worker(cfg, i)
+			wg.Done()
+		}()
 	}
-	defer rdb.Close()
-	err = rdb.Do(ctx, rdb.B().Set().Key("key").Value("val").Build()).Error()
-	if err != nil {
-		log.Fatalf("Failed to set key: %v", err)
-	}
-	val, err := rdb.Do(ctx, rdb.B().Get().Key("key").Build()).ToString()
-	if err != nil {
-		log.Fatalf("Failed to get key: %v", err)
-	}
-	fmt.Printf("Got value: %s\n", val)
 }
